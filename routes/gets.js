52f8d9388decacc49a78005665819eae1c8cb795
mdedeu <mdedeu@itba.edu.ar>
2022-02-24 18:39:06 -0300

    get from DB

--- routes/gets.js
+++ routes/gets.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const Post = require('../models/Post')
 
 const router = express.Router()
 
-router.get('/',(req, res) =>{
-    res.send(photos)
+router.get('/', async (req, res) => {
+    const posts = await Post.find();
+    res.send(posts)
 })
-
 
 module.exports = router
